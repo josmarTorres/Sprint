1e891f42ead73d1093fd909c9ced8f6ead75af16
Josmar Torres <jutorresmir@gmail.com>
2021-05-31 21:03:19 -0500

    Api plomeria

--- api/Plomeria.js
+++ api/Plomeria.js
@@ -4,35 +4,53 @@ const Plomeria = [
       avatar: 'https://media-adsa.camilyo.software/media-adsa/static/3681/273.jpg',
       origin: 'asset',
       name: 'Destape de drenajes',
-      desc: 'Destapamos Coladeras Registros, Bajadas, Trampas de Grasa, limpieza de Fosas Septicas',
+      desc: 'Destapamos Coladeras Registros, Bajadas, Trampas de Grasa',
       precio: '800',
       detalles: ''
     },
     {
-      key: 1,
+      key: 2,
       avatar: 'https://media-adsa.camilyo.software/media-adsa/static/3681/273.jpg',
       origin: 'asset',
       name: 'Instalacion de regadera',
       desc: 'Instalacion de regadera',
-      precio: '800',
+      precio: '500',
       detalles: ''
     },
     {
-        key: 1,
+        key: 3,
         avatar: 'https://media-adsa.camilyo.software/media-adsa/static/3681/273.jpg',
         origin: 'asset',
         name: 'Lavado de cisternas',
         desc: '',
-        precio: '800',
+        precio: '900',
         detalles: ''
     },
       {
-        key: 1,
+        key: 4,
         avatar: 'https://media-adsa.camilyo.software/media-adsa/static/3681/273.jpg',
         origin: 'asset',
-        name: 'Destape de drenajes',
-        desc: 'Destapamos Coladeras Registros, Bajadas, Trampas de Grasa, limpieza de Fosas Septicas',
-        precio: '800',
+        name: 'Instalacion de WC',
+        desc: 'La mejor instalacion de wc para tu baño en el hogar',
+        precio: '500',
+        detalles: ''
+      },
+      {
+        key: 5,
+        avatar: 'https://media-adsa.camilyo.software/media-adsa/static/3681/273.jpg',
+        origin: 'asset',
+        name: 'Reparacion de lavabo',
+        desc: 'La solucion a cada daño que tenga el lavabo',
+        precio: '500',
+        detalles: ''
+      },
+      {
+        key: 6,
+        avatar: 'https://media-adsa.camilyo.software/media-adsa/static/3681/273.jpg',
+        origin: 'asset',
+        name: 'Instalacion de calentador',
+        desc: 'Instalacion de calentador para ahorrar energia, somos lo mejor',
+        precio: '1200',
         detalles: ''
       }
 
